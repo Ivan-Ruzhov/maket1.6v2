b6a6a78306576245341c95e264bfc05d02e9140b
Ivan Ryzhov <xXlAlflXx@gmail.com>
2022-12-15 00:07:36 +0300

    1

--- src/js/order-call/order-call.js
+++ src/js/order-call/order-call.js
@@ -2,7 +2,8 @@ const button = document.querySelector('.side-menu__button-call');
 const buttonTwo = document.querySelector('.menu__button-call');
 const elementHidden = document.querySelector('.order-call');
 const buttonClose = document.querySelector('.order-call__button-close');
-button.addEventListener('click', function () {
+const container = document.querySelector('.order-call__overlay');
+button.addEventListener('click' , function () {
   elementHidden.classList.remove('order-call-hidden')
 });
 buttonTwo.addEventListener('click', function () {
@@ -11,3 +12,12 @@ buttonTwo.addEventListener('click', function () {
 buttonClose.addEventListener('click', function () {
   elementHidden.classList.add('order-call-hidden')
 });
+document.addEventListener('keydown', function(evt) {
+  if(evt.keyCode === 27) {
+    elementHidden.classList.add('order-call-hidden')
+  }
+})
+container.addEventListener('click', function() {
+  elementHidden.classList.add('order-call-hidden')
+})
+
--- src/js/side-menu/side-menu.js
+++ src/js/side-menu/side-menu.js
@@ -1,7 +1,7 @@
 const buttonMenu = document.querySelector('.menu__button-burger');
 const ButtonSideMenu = document.querySelector('.side-menu__button-burger')
 const sideMenu = document.querySelector('.side-menu');
-
+const container = document.querySelector('.side-menu__overlay')
 buttonMenu.addEventListener('click', function() {
   sideMenu.classList.remove('side-menu-hidden')
 })
@@ -11,4 +11,14 @@ ButtonSideMenu.addEventListener('click', function() {
 if (document.documentElement.clientWidth >= 1440) {
   sideMenu.classList.remove('side-menu-hidden');
   buttonMenu.classList.add('menu__button-burger-hidden');
+}
+if (document.documentElement.clientWidth < 1440) {
+  document.addEventListener('keydown', function(evt) {
+    if(evt.keyCode === 27) {
+      sideMenu.classList.add('side-menu-hidden')
+    }
+  })
+  container.addEventListener('click', function() {
+    sideMenu.classList.add('side-menu-hidden')
+  })
 }
